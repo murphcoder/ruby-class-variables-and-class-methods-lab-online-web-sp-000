d3390d310ea7f2421522b44c90b66277220166b0
Sean Murphy <filmmakersean@fastmail.net>
2019-07-15 18:58:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,32 +7,4 @@ class Song
   
   attr_accessor :song, :artist, :genre
   
-  def initialize(song, artist, genre)
-    @song = song
-    @artist = artist
-    @genre = genre
-    @@count =+
-    
-    if @@artists.none? {|entry| entry == artist}
-      @@artists << artist
-    end
-    
-    if @@genres.none? {|entry| entry == genre}
-      @@genres << genre
-    end
-    
-    if @@genre_count[genre] = nil
-      @@genre_count[genre] = 1
-    else
-      @@genre_count[genre] =+
-    end
-  
-  def artists
-    @@artists
-  end
-  
-  def genres
-    @@genres
-  end
-end
 end
