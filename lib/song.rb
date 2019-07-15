2f65577afc51d3c9db789b050b06119ad45df52b
Sean Murphy <filmmakersean@fastmail.net>
2019-07-15 19:31:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
   
   def self.genre_count
     @@genres.each do |genre|
-      if @@genre_count[genre] = nil
+      if @@genre_count[genre] == nil
         @@genre_count[genre] = 1
       else
         @@genre_count[genre] += 1
